80d52acbe9cb1a566e07855ca41f19bf112d4c53
lisovenko02 <vanyalisovenkoo@gmail.com>
2024-10-23 11:06:33 +0300

    add generateMetadata

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -8,6 +8,26 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { chatId: string }
+}) {
+  const session = await getServerSession(authOptions)
+  if (!session) notFound()
+  const [userId1, userId2] = params.chatId.split('--')
+  const { user } = session
+
+  const chatPartnerId = user.id === userId1 ? userId2 : userId1
+  const chatPartnerRaw = (await fetchRedis(
+    'get',
+    `user:${chatPartnerId}`
+  )) as string
+  const chatPartner = JSON.parse(chatPartnerRaw) as User
+
+  return { title: `FriendZone | ${chatPartner.name} chat` }
+}
+
 type ChatWithSomebodyProps = {
   params: {
     chatId: string
